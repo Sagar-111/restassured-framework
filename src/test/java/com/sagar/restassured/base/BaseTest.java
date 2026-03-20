1858e603251df07f51129af9898591e5b9831f25
Sagar <sgrchdhr82@gmail.com>
2026-03-20 18:41:00 +0530

    feat: add ConfigReader, BaseTest and testng.xml - ConfigReader loads config.properties using static block - BaseTest set up RequestSpecification for petstore and ReqRes - RequestLoggingFilter and ResponseLoggingFilter added for debugging - testng.xml suite configured for petstore and packages - Moved base and config to test scope to align with dependency scoping

---
+++ src/test/java/com/sagar/restassured/base/BaseTest.java
@@ -0,0 +1,44 @@
+package com.sagar.restassured.base;
+
+import com.sagar.restassured.config.ConfigReader;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+import org.testng.annotations.BeforeSuite;
+
+public class BaseTest {
+
+    protected static RequestSpecification petstoreSpec;
+    protected static RequestSpecification reqresSpec;
+    protected static ResponseSpecification responseSpec;
+
+    @BeforeSuite
+    public void setupSpec(){
+
+        // Petstore request specification
+        petstoreSpec = new RequestSpecBuilder()
+                .setBaseUri(ConfigReader.getPetstoreBaseUrl())
+                .setContentType(ContentType.JSON)
+                .addFilter(new RequestLoggingFilter())
+                .addFilter(new ResponseLoggingFilter())
+                .build();
+
+        // ReqRes request specification
+        reqresSpec = new RequestSpecBuilder()
+                .setBaseUri(ConfigReader.getReqresBaseUrl())
+                .setContentType(ContentType.JSON)
+                .addFilter(new ResponseLoggingFilter())
+                .addFilter(new RequestLoggingFilter())
+                .build();
+
+        // Common response specification
+        responseSpec = new ResponseSpecBuilder()
+                .expectContentType(ContentType.JSON)
+                .build();
+    }
+
+}
